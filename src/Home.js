a83184f12babc377de0f79a6fd5a0668438d1c01
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-10-21 01:47:37 +0200

    Create app components

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            Home
+            {/* 
+                Header
+                Weeks List
+                Footer
+            */}
+        </div>
+    )
+}
+
+export default Home
+Home
---
+++ src/Week.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Week() {
+    return (
+        <div>
+            Week
+            {/* 
+                Header
+                Week Content
+                Footer
+            */}
+        </div>
+    )
+}
+
+export default Week
