953aa0e1453b600c929cbf86d33612dfec58d256
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-10-21 21:08:53 +0200

    Show Track link

--- src/components/WeekContent.js
+++ src/components/WeekContent.js
@@ -7,17 +7,26 @@ import Shorts from './Shorts'
 function WeekContent({weekNumber}) {
     //Selected Week
     const week = weeks[weekNumber];
+    const finalWeek = (Object.keys(weeks).length) - 1 == weekNumber ;
+    console.log(weekNumber)
     return (
         <div>
             <header>
                 <h1>Week {weekNumber} - <span>{week.name}</span></h1>
             </header>
-
+            
             <LectureVideo youtube_video = {week.youtube_video}/>
-            <LectureFiles weekNumber ={weekNumber} />
-            <Shorts weekNumber ={weekNumber}/>
 
-            <li><a href={`https://cs50.harvard.edu/x/2020/psets/${weekNumber}/`}>Problem Set {weekNumber}</a></li>
+            <LectureFiles weekNumber ={weekNumber} />
+
+            {  weeks[weekNumber].shorts && <Shorts weekNumber ={weekNumber}/>}
+
+
+            <div>
+                <a href={`https://cs50.harvard.edu/x/2020/psets/${weekNumber}/`}>
+                    Problem Set {weekNumber} { finalWeek && " (Choose your Track)"}
+                </a>
+            </div>
 
         </div>
     )
