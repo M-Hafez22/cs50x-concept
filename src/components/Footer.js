dc1af641955b21d229ef47121726a94905141ea1
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-10-21 02:47:35 +0200

    Add Footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import footerData from '../data/footerLinks'
 
 function Footer() {
+    // Loop through Lists
+    const lists = Object.keys(footerData).map(key => (
+        <ul key={key}>
+            {   
+                //Loop inside every list       
+                (footerData[key].map(i => (
+                    <li><a key={i.name} href={i.link}>{i.name}</a></li>
+                )))
+            }
+            {/* Add seperator between  lists */}
+            <hr></hr>
+        </ul>
+    ))
+
     return (
         <div>
-            Page Footer
+            {lists}
         </div>
     )
 }
