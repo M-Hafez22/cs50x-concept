6d0ba7ff041be2704857b256432e711713adceb3
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-10-21 02:54:13 +0200

    Add Home content

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import {Link} from 'react-router-dom';
+import weeks from './data/weeks.json'
+
 
 
 function Home() {
+
+    const WeeksList = Object.keys(weeks).map((key, index )=> (
+        <li>
+            <Link to={`/week/${index}`} > Week {key} - {weeks[index].name}</Link>
+        </li>
+    ))
     return (
         <div>
-        Home
             <Header />
-            {/* 
-                Weeks List
-            */}
+            <ul>
+                {WeeksList}
+            </ul>
             <Footer />
         </div>
     )
