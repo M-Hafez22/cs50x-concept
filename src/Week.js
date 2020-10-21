b435b44f2a6cf4870dbdb79316839fc31dfd2155
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-10-21 17:58:26 +0200

    Add required data

--- src/Week.js
+++ src/Week.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
-
+import WeekContent from './components/WeekContent'
+import { useParams } from 'react-router'
+import weeks from './data/weeks'
 
 function Week() {
+    const {number} = useParams();
+    const week = weeks[number];
     return (
         <div>
-            Week
             <Header />
-            {/* 
-                Week Content
-            */}
+            <WeekContent 
+                weekNumber = {number}
+                weekName = {week.name}
+                youtube_video = {week.youtube_video}
+            /> 
             <Footer />
         </div>
     )
