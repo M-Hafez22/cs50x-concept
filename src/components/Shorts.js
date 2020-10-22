306bdfc48b1d8daa8ebcf0cb3449293edfe0e18a
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-10-22 02:22:13 +0200

    Style shorts

--- src/components/Shorts.js
+++ src/components/Shorts.js
@@ -7,14 +7,14 @@ function Shorts({ weekNumber }) {
     const shorts = weeks[weekNumber].shorts;
     const shortsList = shorts && 
     (
-            <ul>
+            <ul className="shorts-list">
                 {Object.keys(shorts).map((key) => (
                     <li><a href={shorts[key]}>{key}</a></li>
                 ))}
             </ul>   
     );
     return (
-        <div>
+        <div className="shorts">
             <h2>Shorts</h2>
             {shortsList}
         </div>
