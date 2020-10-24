ee5f368fe9f0ad11e77d9eebc8dd74fd8a6dd3d4
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-10-25 00:54:53 +0200

    Set the lecture video dimensions

--- src/components/LectureVideo.js
+++ src/components/LectureVideo.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 function LectureVideo({youtube_video}) {
     return (
-        <iframe
-            src={youtube_video}
-            frameborder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-            allowfullscreen>
-        </iframe>
+        <div className='lecture-video'>
+            <iframe
+                src={youtube_video}
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+            </iframe>
+        </div>
     )
 }
 
