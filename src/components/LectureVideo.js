c6a1d3c8e960856632a8b3cae5cab8c6bbe827fb
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-10-25 21:33:35 +0200

    Add iframe title

--- src/components/LectureVideo.js
+++ src/components/LectureVideo.js
@@ -5,6 +5,7 @@ function LectureVideo({youtube_video}) {
         <div className='lecture-video'>
             <iframe
                 src={youtube_video}
+                title="Lecture"
                 frameborder="0" 
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                 allowfullscreen>
